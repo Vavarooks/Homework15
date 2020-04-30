1622dfe15199f0485122f652e573c90564ad578e
Vavarooks <vavarooks123@gmail.com>
2020-04-30 16:20:28 -0700

    starter code.

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var PORT = process.env.PORT|| 8080;
+var app = express();
+// var apirouts = require("./routes/apirouts");
+// var htmlrouts = require("./routes/htmlroutes");
+app.use(express.urlencoded({extended : true}));
+app.use(express.json())
+app.use(express.static("public"))
+mongoose.connect("mongodb://localhost/FitnessTracker", {
+    useNewUrlParser : true, 
+    useFindAndModify : false
+})
+
+
+
+
+
+
+
+app.listen(PORT, function()
+    {
+console.log("App is listening on port", PORT)
+    })
